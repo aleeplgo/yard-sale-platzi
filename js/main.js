4165b6670f531483263bb27d8614ccfb61965c4c
“M4 <m4estudio.qro@gmail.com>
2022-10-19 01:43:50 -0500

    Fusionamos el menu hamburguesa con nuestro archivo principal y se activo el evento para mostrar y ocultar el menu al dar click

--- js/main.js
+++ js/main.js
@@ -4,6 +4,24 @@ const navbarEmailClick = document.querySelector(".navbar-email").addEventListene
 const navbarEmailOver = document.querySelector(".navbar-email").addEventListener("mouseover", toggleDropdownMenu);
 const dropdownMenu = document.querySelector(".desktop-menu");
 
+
 function toggleDropdownMenu() {
     return dropdownMenu.classList.toggle("inactive");
+}
+
+
+/*MOSTRAR BURGER MENU EN MOBILE*/
+const iconMenuClick = document.querySelector(".icon-menu").addEventListener("click", toggleMobileMenu);
+const iconMenuOver = document.querySelector(".icon-menu").addEventListener("mouseover", toggleMobileMenu);
+const mobileMenu = document.querySelector(".mobile-menu");
+const closeMobilemenuClick = document.querySelector(".close-mobilemenu").addEventListener("click", closeMobileMenu);
+const closeMobilemenuOut = document.querySelector(".close-mobilemenu").addEventListener("mouseout", closeMobileMenu);
+
+
+function toggleMobileMenu() {
+    return mobileMenu.classList.toggle("inactive");
+}
+
+function closeMobileMenu() {
+    return mobileMenu.classList.add("inactive");
 }
