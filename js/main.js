0f5520d230ffd740423f9e363df3002fa57c88cd
“M4 <m4estudio.qro@gmail.com>
2022-10-20 22:01:07 -0500

    Se agregó con evento click el desgloce del cartito y al dar click en el primer producto se despliega el detalle del producto

--- js/main.js
+++ js/main.js
@@ -6,7 +6,13 @@ const dropdownMenu = document.querySelector(".desktop-menu");
 
 
 function toggleDropdownMenu() {
-    return dropdownMenu.classList.toggle("inactive");
+    const isAsideCartClosed = shoppingCartAside.classList.contains("inactive");
+
+    if (!isAsideCartClosed) {
+        return shoppingCartAside.classList.add("inactive");
+    } else {
+        return dropdownMenu.classList.toggle("inactive");
+    }
 }
 
 
@@ -19,9 +25,65 @@ const closeMobilemenuOut = document.querySelector(".close-mobilemenu").addEventL
 
 
 function toggleMobileMenu() {
-    return mobileMenu.classList.toggle("inactive");
+    const isAsideCartClosed = shoppingCartAside.classList.contains("inactive");
+
+    if (!isAsideCartClosed) {
+        return shoppingCartAside.classList.add("inactive");
+    } else {
+        return mobileMenu.classList.toggle("inactive");
+    }
 }
 
 function closeMobileMenu() {
     return mobileMenu.classList.add("inactive");
+}
+
+/*MOSTRAR MENU CARRITO */
+const navbarShoppingCartClick = document.querySelector(".navbar-shopping-cart").addEventListener("click", toggleCartAside);
+const navbarShoppingCartOver = document.querySelector(".navbar-shopping-cart").addEventListener("mouseover", toggleCartAside);
+const shoppingCartAside = document.querySelector(".shopping-cart-aside");
+
+function toggleCartAside() {
+    dropdownMenu.classList.add("inactive");
+    const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+    const isProductDetailAsideClosed = productDetailAside.classList.contains("inactive");
+    const isDropdownMenuClosed = dropdownMenu.classList.contains("inactive");
+
+
+    if (!isMobileMenuClosed) {
+        return mobileMenu.classList.add("inactive");
+    } else if (!isProductDetailAsideClosed) {
+        return shoppingCartAside.classList.add("inactive");
+    } else if (!isDropdownMenuClosed) {
+        return dropdownMenu.classList.add("inactive");
+    } else {
+        return shoppingCartAside.classList.toggle("inactive");
+    }
+}
+
+/*MOSTRAR PRODUCTO DETAIL */
+const productCard = document.querySelector(".product-card").addEventListener("click", toggleProductDetail);
+const productDetailAside = document.querySelector(".product-detail");
+const iconProductDetailClosed = document.querySelector(".product-detail-close").addEventListener("click", closeProductDetail);
+
+
+function toggleProductDetail() {
+    const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+    const isAsideCartClosed = shoppingCartAside.classList.contains("inactive");
+    const isDropdownEmailClosed = dropdownMenu.classList.contains("inactive");
+
+    if (!isMobileMenuClosed) {
+        return mobileMenu.classList.add("inactive");
+    } else if (!isAsideCartClosed) {
+        return shoppingCartAside.classList.add("inactive");
+    } else if (!isDropdownEmailClosed) {
+        return dropdownMenu.classList.add("inactive");
+    } else {
+        return productDetailAside.classList.toggle("inactive");
+    }
+
+}
+
+function closeProductDetail() {
+    return productDetailAside.classList.add("inactive");
 }
